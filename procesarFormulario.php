691993b6c9a8cab2c5f483789fb576120bf5a618
Diego Fernando Roser <Diego Fernando Roser@Diego>
2015-07-28 16:57:37 -0500

    segunda actualizacion

--- procesarFormulario.php
+++ procesarFormulario.php
@@ -3,11 +3,14 @@ $nombre = filter_input(INPUT_POST, 'inputNombre');
 $genero = filter_input(INPUT_POST, 'radio');
 $edad =  filter_input(INPUT_POST, 'inputEdad');
  if($edad>= 18 and $edad < 22){
-     echo "Hola : "+$nombre." bienvenido(a) a la Chiquiteca";
+     echo "Hola : ".$nombre." bienvenido(a) a la Chiquiteca";
                               }
  if($edad>= 22 and $edad < 40){
-     echo "Hola : "+$nombre." bienvenido(a) a la Discoteca";
+     echo "Hola : ".$nombre." bienvenido(a) a la Discoteca";
                               }
- if(edad>=40){
-     header('Location: entradaViejoteca.php');
+ if($edad>=40){
+     if($genero=="masculino"){
+         header('Location: entradaViejotecaHombre.php');
+                             }
+     
              }
