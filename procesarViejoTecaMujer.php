f2089758bcb4e450156d024d07c072d939ed707b
Diego Fernando Roser <Diego Fernando Roser@Diego>
2015-07-29 08:57:42 -0500

    segunda actualizacion

--- procesarViejoTecaMujer.php
+++ procesarViejoTecaMujer.php
@@ -1,4 +1,14 @@
 <?php
 
+$blusa = filter_input(INPUT_POST, 'blusa');
+$pantalon = filter_input(INPUT_POST, 'pantalon');
+$zapatos = filter_input(INPUT_POST, 'Zapatos');
 
+if($blusa=="Blusa escotada" and ($pantalon=="Falda corta" or $pantalon=="Pantalon de tela")and ($zapatos=="Tacones" or $zapatos=="Serio cerrado")){
+    echo "Usted cumple con todos los requisitos, bienvenida a la viejoteca!";
+                         }
+ else {
+    
+    echo "Lo sentimos, su atuendo no es el adecuado";
+      }                         
 
